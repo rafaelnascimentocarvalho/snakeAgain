947c45a789e93a6bebb0ddd6831d3d04b6dc1c0a
rafael8p <rafaelcarvalhosjrp@gmail.com>
2024-01-06 00:43:06 -0300

    'up'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Template from "@/src/default/template";
 import { useState, useEffect } from "react";
 
 interface SnakeSegment {
@@ -131,79 +132,81 @@ const Home: React.FC = () => {
   }, [snake, direction, queuedDirection, apple]);
 
   return (
-    <div className="grid py-10 px-6 text-white max-w-[32rem] mx-auto w-full gap-4">
-      <div>SCORE: {snake.length}</div>
-      <div className="grid grid-cols-20 gap-y-1 w-full">
-        {Array.from({ length: GRID_SIZE }).map((_, rowIndex) => (
-          <div key={rowIndex} className="flex gap-x-1 w-full">
-            {Array.from({ length: GRID_SIZE }).map((_, colIndex) => (
-              <div
-                key={colIndex}
-                className={`w-full relative rounded-sm bg-gray-900`}
-              >
-                <div className="aspect-[1/1] relative">
-                  <div
-                    className={`w-full h-full rounded-sm absolute top-0 left-0 ease ${
-                      snake.some(
-                        (segment) =>
-                          segment.x === colIndex && segment.y === rowIndex
-                      )
-                        ? "bg-green-400"
-                        : apple.x === colIndex && apple.y === rowIndex
-                        ? "bg-red-500 border-2 border-red-400"
-                        : ""
-                    }`}
-                  ></div>
+    <Template>
+      <div className="grid py-10 px-6 text-white max-w-[32rem] mx-auto w-full gap-4">
+        <div>SCORE: {snake.length}</div>
+        <div className="grid grid-cols-20 gap-y-1 w-full">
+          {Array.from({ length: GRID_SIZE }).map((_, rowIndex) => (
+            <div key={rowIndex} className="flex gap-x-1 w-full">
+              {Array.from({ length: GRID_SIZE }).map((_, colIndex) => (
+                <div
+                  key={colIndex}
+                  className={`w-full relative rounded-sm bg-gray-900`}
+                >
+                  <div className="aspect-[1/1] relative">
+                    <div
+                      className={`w-full h-full rounded-sm absolute top-0 left-0 ease ${
+                        snake.some(
+                          (segment) =>
+                            segment.x === colIndex && segment.y === rowIndex
+                        )
+                          ? "bg-green-400"
+                          : apple.x === colIndex && apple.y === rowIndex
+                          ? "bg-red-500 border-2 border-red-400"
+                          : ""
+                      }`}
+                    ></div>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
-      <div className="flex justify-end pt-10">
-        <div></div>
-        <div className="flex items-center gap-4">
-          <button
-            onTouchStart={() => touch("LEFT")}
-            onClick={() => touch("LEFT")}
-            className="w-full p-6 bg-gray-900 rounded-md relative"
-          >
-            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -mt-[2px]">
-              {"<"}
-            </span>
-          </button>
-          <div className="w-full grid gap-16">
+              ))}
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-end pt-10">
+          <div></div>
+          <div className="flex items-center gap-4">
             <button
-              onTouchStart={() => touch("UP")}
-              onClick={() => touch("UP")}
+              onTouchStart={() => touch("LEFT")}
+              onClick={() => touch("LEFT")}
               className="w-full p-6 bg-gray-900 rounded-md relative"
             >
-              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 ml-1">
+              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -mt-[2px]">
                 {"<"}
               </span>
             </button>
+            <div className="w-full grid gap-16">
+              <button
+                onTouchStart={() => touch("UP")}
+                onClick={() => touch("UP")}
+                className="w-full p-6 bg-gray-900 rounded-md relative"
+              >
+                <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 ml-1">
+                  {"<"}
+                </span>
+              </button>
+              <button
+                onTouchStart={() => touch("DOWN")}
+                onClick={() => touch("DOWN")}
+                className="w-full p-6 bg-gray-900 rounded-md relative"
+              >
+                <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 ml-1">
+                  {">"}
+                </span>
+              </button>
+            </div>
             <button
-              onTouchStart={() => touch("DOWN")}
-              onClick={() => touch("DOWN")}
+              onTouchStart={() => touch("RIGHT")}
+              onClick={() => touch("RIGHT")}
               className="w-full p-6 bg-gray-900 rounded-md relative"
             >
-              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rotate-90 ml-1">
+              <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -mt-[2px]">
                 {">"}
               </span>
             </button>
           </div>
-          <button
-            onTouchStart={() => touch("RIGHT")}
-            onClick={() => touch("RIGHT")}
-            className="w-full p-6 bg-gray-900 rounded-md relative"
-          >
-            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -mt-[2px]">
-              {">"}
-            </span>
-          </button>
         </div>
       </div>
-    </div>
+    </Template>
   );
 };
 
